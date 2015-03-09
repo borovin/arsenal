3db175cfe3405d77616c510e39d1e86973c61a8a
Andrew Borovin <a@borovin.com>
2015-03-10 00:36:26 +0300

    tabs

--- src/kit/page/page.js
+++ src/kit/page/page.js
@@ -3,6 +3,29 @@ define(function (require) {
     var Block = require('kit/block/block');
 
     return Block.extend({
-        el: document.body
+        el: document.body,
+        events: {
+            'click .page__tab': function(e){
+
+                e.preventDefault();
+
+                var $tab = $(e.currentTarget),
+                    $tabContent = $('.page__tabContent[data-tab="' + e.currentTarget.dataset.tab + '"]');
+
+                $tab
+                    .addClass('page__tab_active')
+                    .siblings('.page__tab_active')
+                    .removeClass('page__tab_active');
+
+                $tabContent
+                    .addClass('page__tabContent_active')
+                    .siblings('.page__tabContent_active')
+                    .removeClass('page__tabContent_active');
+
+                $tabContent
+                    .find('[autofocus]')
+                    .focus();
+            }
+        }
     });
 });
